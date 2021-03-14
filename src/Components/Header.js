4192bbb490222531e89667b88de5a120575ce64f
geekrohan <geekboyrohan@gmail.com>
2021-03-14 17:50:45 +0530

    added resume tab in header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 
-const Header = ({delay}) => {
+const Header = ({ delay }) => {
     return (
-        <motion.div 
+        <motion.div
             initial={{
-            opacity: 0,
-            y: -150,
+                opacity: 0,
+                y: -150,
             }}
             animate={{
-            opacity: 1,
-            y: 0,
+                opacity: 1,
+                y: 0,
             }}
             transition={{
-            delay: delay,
-            type: "spring",
-            damping: 8,
+                delay: delay,
+                type: "spring",
+                damping: 8,
             }}
         >
             <nav className='space-x-10'>
@@ -28,6 +28,11 @@ const Header = ({delay}) => {
                     <li>
                         <a href='#contact' className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
                             Contact
+                        </a>
+                    </li>
+                    <li>
+                        <a target="_blank" rel="noreferrer" href='https://firebasestorage.googleapis.com/v0/b/rohan-portfolio-be26d.appspot.com/o/rohan-resume.pdf?alt=media&token=92db2157-d956-4879-bc5a-ea8781e0e56d' className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
+                            Resume
                         </a>
                     </li>
                 </ul>
